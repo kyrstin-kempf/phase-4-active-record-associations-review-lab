27d84e6110582a554a28493278f734d7218a1521
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-11-08 15:54:00 -0500

    commit tree da8e984e53f04eb9e654042b18a5c3ffcaa51f38

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,5 @@
 class Passenger < ApplicationRecord
+    has_many :rides # direct association
+    has_many :taxis, through: :rides # show data THROUGH on the front end
+    #passenger.taxi --> show this data
 end
--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,2 +1,4 @@
 class Taxi < ApplicationRecord
+    has_many :rides # direct association
+    has_many :passengers, through: :rides
 end
